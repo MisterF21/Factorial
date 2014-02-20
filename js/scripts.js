282ecfc4c6699a3275e6aba8290159c23677d20b
Marty Fajardo & Michael Grossman <student@epicodus.com>
2014-02-20 14:08:11 -0800

    add a new function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 var factorial = function(number) {
-  if (number === 0 || number === 1) {
-    return 1; 
-  };
-  var total = number * (number - 1);
-    for (var count = number - 2; count > 1; count--) {
-    
-    console.log(count);
-    console.log(total);
-    console.log('=================');
-
-    total *= count;
-
-    };
-    console.log(total);
-    return total;
+  if (number === 0) {
+    return 1;
+  } else if (number < 0) {
+    alert('Please enter a positive number');
+  } else {
+   return (number * (factorial(number-1)));
+  }
 };
+
+
+// var factorial = function(number) {
+//   if (number === 0 || number === 1) {
+//     return 1; 
+//   };
+//   var total = number * (number - 1);
+//     for (var count = number - 2; count > 1; count--) {
+    
+//     console.log(count);
+//     console.log(total);
+//     console.log('=================');
+
+//     total *= count;
+
+//     };
+//     console.log(total);
+//     return total;
+// };
 
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,15 @@
 describe('factorial', function() {
+
   it('should multiply itself by every integer less than itself consecutively', function() {
-    factorial(1).should.equal(1)
     factorial(7).should.equal(5040)
+  });
+
+  it('should return one if zero is entered as a number', function() {
     factorial(0).should.equal(1)
   });
+
+  it('should return one if one is entered as a number', function() {
+    factorial(1).should.equal(1)
+  });  
+
 });
